af657646a3fe1054c3125a3da4e7149d42576070
Ramses Yael De La Peña Coronado <ramses.delapena@digitalfemsa.com>
2023-08-07 10:49:24 -0600

    feat: add eslint config typescript-parser + prettier in eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,16 @@
 module.exports = {
   root: true,
-  extends: '@react-native',
+  extends: ['@react-native', 'plugin:@typescript-eslint/recommended'],
+  plugins: ['jest', '@typescript-eslint', 'prettier'],
+  parser: '@typescript-eslint/parser',
+  ignorePatterns: [
+    // Files excluded on tsconfig
+    'node_modules',
+    'babel.config.js',
+    'metro.config.js',
+    'jest.config.js',
+  ],
+  rules: {
+    'prettier/prettier': 'error',
+  },
 };
