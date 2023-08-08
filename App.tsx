c12e31c0c203944ccd03453b793e5fd8f19829b7
Rubén Capetillo <jose.capetillo@digitalfemsa.com>
2023-08-07 19:47:09 -0600

    feat(*): internationalization implemented with i18n

--- App.tsx
+++ App.tsx
@@ -2,6 +2,8 @@ import 'react-native-gesture-handler';
 import React from 'react';
 import {SafeAreaView} from 'react-native';
 
+import './services/i18n';
+
 import {ThemeProvider} from '@digitaltitransversal';
 import {AppProvider} from '@sas/context';
 import {RootNavigation} from '@sas/navigation/root-navigation';
--- src/screens/home-screen/home-screen.tsx
+++ src/screens/home-screen/home-screen.tsx
@@ -2,16 +2,21 @@ import React from 'react';
 import {View, Text} from 'react-native';
 
 import {BottomTabScreenProps} from '@react-navigation/bottom-tabs';
+import {useTranslation} from 'react-i18next';
 
 import {MainContainer} from '@sas/components';
 import {RootStackParamList} from '@sas/navigation/navigation.types';
 
 export type HomeScreenProps = BottomTabScreenProps<RootStackParamList>;
 
-export const HomeScreen = (_props: HomeScreenProps) => (
-  <MainContainer>
-    <View>
-      <Text>HomeScreen</Text>
-    </View>
-  </MainContainer>
-);
+export const HomeScreen = (_props: HomeScreenProps) => {
+  const {t} = useTranslation();
+
+  return (
+    <MainContainer>
+      <View>
+        <Text>{t('welcome')}</Text>
+      </View>
+    </MainContainer>
+  );
+};
