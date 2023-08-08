2d11311c5837c18e46615622c6d4b9884f0faca1
Ramses Yael De La Peña Coronado <ramses.delapena@digitalfemsa.com>
2023-08-08 17:19:17 -0600

    feat: add text for movements list tabs

--- src/navigation/authenticated-navigation/movements-tab-navigation.tsx
+++ src/navigation/authenticated-navigation/movements-tab-navigation.tsx
@@ -1,8 +1,11 @@
+/* eslint-disable react/no-unstable-nested-components */
 /* eslint-disable react-native/no-inline-styles */
 import React from 'react';
 
 import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
+import {Trans} from 'react-i18next';
 
+import {Text} from '@digitaltitransversal';
 import {RootStackParamList, RouteNames} from '@sas/navigation/navigation.types';
 import {
   MovementsEarnedScreen,
@@ -29,14 +32,35 @@ export const MovementsTabNavigation = () => (
     <Tab.Screen
       name={RouteNames.MovementsListTab}
       component={MovementsListScreen}
+      options={{
+        tabBarLabel: () => (
+          <Text>
+            <Trans i18nKey="movements.tabs.all" />
+          </Text>
+        ),
+      }}
     />
     <Tab.Screen
       name={RouteNames.MovementsEarnedTab}
       component={MovementsEarnedScreen}
+      options={{
+        tabBarLabel: () => (
+          <Text>
+            <Trans i18nKey="movements.tabs.earned" />
+          </Text>
+        ),
+      }}
     />
     <Tab.Screen
       name={RouteNames.MovementsUsedTab}
       component={MovementsUsedScreen}
+      options={{
+        tabBarLabel: () => (
+          <Text>
+            <Trans i18nKey="movements.tabs.used" />
+          </Text>
+        ),
+      }}
     />
   </Tab.Navigator>
 );
