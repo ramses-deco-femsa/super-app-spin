7344cc3fa04e5742ab0a1fe0cc472162b7d0f628
Ramses Yael De La Peña Coronado <ramses.delapena@digitalfemsa.com>
2023-08-09 14:31:23 -0600

    feat: add more brand images

--- src/constants/movements-icon-mapper.ts
+++ src/constants/movements-icon-mapper.ts
@@ -4,4 +4,6 @@ export const MOVEMENTS_ICON_MAPPER: {[key in string]: ImageProps['source']} = {
   ['Oxxo']: require('../../assets/images/oxxo.png'),
   ['Oxxo Gas']: require('../../assets/images/oxxo-gas.png'),
   ['Volaris']: require('../../assets/images/volaris.png'),
+  ['Smart Fit']: require('../../assets/images/smart-fit.png'),
+  ['VIX']: require('../../assets/images/vix.png'),
 };
